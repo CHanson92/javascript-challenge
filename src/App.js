d4600b701d8c8ddf2cca1d0cda3f1b607009941d
Chris Hanson <BlondyHanson@gmail.com>
2019-03-28 19:25:56 +0000

    Created components to get the geojson as props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+/* eslint-disable import/no-named-as-default */
 import React, { Component } from 'react';
 import MapContainer from './components/MapContainer';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('/data/boat_ramps.geojson')
+      .then(response => response.json()).then((data) => {
+        this.setState({ markers: data });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <MapContainer />
+        <MapContainer {...this.state} />
       </div>
     );
   }
--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable react/no-string-refs */
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const mapStyles = {
   width: '75%',
@@ -7,17 +9,46 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+  componentDidUpdate() {
+    const google = window.google;
+    this.map = new google.maps.Map(this.refs.map, {
+      center: this.props.center,
+      zoom: 4,
+    });
+
+    this.createMarkers(this.props.markers);
+  }
+
+  createMarkers(users) {
+    const google = window.google;
+
+    users.map((user) => {
+      this.marker = new google.maps.Marker({
+        position: {
+          lat: user.location.latitude,
+          lng: user.location.longitude,
+        },
+        map: this.map,
+      });
+      this.state.markers.push(this.marker);
+    });
+  }
+
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233,
-        }}
-      />
+        <Map
+          google={this.props.google}
+          zoom={5}
+          style={mapStyles}
+          initialCenter={{
+            lat: -28.0167,
+            lng: 153.4000,
+          }}>
+          <Marker
+            title="Marker"
+            name="Ship"
+          />
+        <Map/>
     );
   }
 }
